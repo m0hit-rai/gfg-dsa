da1f9d7e536def28ca7b444362ed41ce09bd0320
Mohit Rai <mohitrairobu@gmail.com>
2021-09-29 13:06:11 +0530

    graph

---
+++ GraphAll.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+class GraphQuestions
+{
+	
+	public:
+	void add_edge_undirected(vector<int>graph[],int u, int v)
+	{
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+	void create_graph(vector<int>graph[])
+	{
+		// add_edge_undirected(graph,0,1);
+		// add_edge_undirected(graph,0,2);
+		// add_edge_undirected(graph,0,5);
+		// add_edge_undirected(graph,1,3);
+		// add_edge_undirected(graph,2,4);
+		// add_edge_undirected(graph,3,5);
+		// add_edge_undirected(graph,4,5);
+		// ---------------------------
+		add_edge_undirected(graph,0,2);
+		add_edge_undirected(graph,0,1);
+		add_edge_undirected(graph,1,2);
+		add_edge_undirected(graph,3,4);
+		add_edge_undirected(graph,5,6);
+		add_edge_undirected(graph,5,7);
+		add_edge_undirected(graph,7,8);
+	}
+	void bfs(vector<int>graph[],int i,int vertex,vector<bool>&visited)
+	{
+		// unordered_set<int> s;
+		// vector<bool> visited(vertex,false);
+		queue<int> q;
+		q.push(i);
+		visited[i]=true;
+		// s.insert(i);
+		// cout<<i<<"\t";
+		while (!q.empty())
+		{
+			int x=q.front();
+			q.pop();
+			cout<<x<<"\t";
+			for(auto it :graph[x])
+			{
+				// if(s.count(it)==0)
+				if(!visited[it])
+				{
+					// s.insert(it);
+					// cout<<it<<"\t";
+					visited[it]=true;
+					q.push(it);
+				}
+			}
+		}
+	}
+	void BFS_disconnected(vector<int> graph[],int vertex)
+	{
+		vector<bool> visited(vertex,false);
+		int connected_component=0;
+		for(int i=0;i<vertex;i++)
+		{
+			if(!visited[i])
+			{
+				connected_component++;
+				bfs(graph,i,vertex,visited);
+			}
+		}
+		cout<<"Connected Component = "<<connected_component;
+	}
+	void DFS_Rec(vector<int>graph[],vector<bool> &visited,int i)
+	{
+		if(!visited[i])
+		{
+			cout<<i<<"\t";
+			visited[i]=true;
+			for(auto it : graph[i]) DFS_Rec(graph,visited,it);
+		}
+	}
+	void DFS(vector<int> graph[],int vertex)
+	{
+		vector<bool>visited(10,false);
+		for(int i=0;i<vertex;i++)
+		{
+			if(!visited[i]) DFS_Rec(graph,visited,i);
+		}
+	}
+};
+int main()
+{
+	vector<int>graph[10];
+	GraphQuestions obj;
+	obj.create_graph(graph);
+	// obj.bfs(graph,0,6);
+	cout<<"BFS : "<<"\t";
+	obj.BFS_disconnected(graph,9);
+	cout<<"\nDFS : "<<"\t";
+	obj.DFS(graph,9);
+}
