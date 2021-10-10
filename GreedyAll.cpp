fbcca36716b206de234b1b4730b851645d944358
Mohit Rai <mohitrairobu@gmail.com>
2021-10-10 20:24:25 +0530

    job sequencing

--- GreedyAll.cpp
+++ GreedyAll.cpp
@@ -1,5 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct jobs
+{
+	int job_number,deadline,profit;
+	jobs()
+	{
+		job_number=-1;
+		deadline=-1;
+		profit=-1;
+	}
+	jobs(int j,int d,int p)
+	{
+		job_number=j;
+		deadline=d;
+		profit=p;
+	}
+};
 class GreedyQuestions
 {
 	public:
@@ -69,6 +85,51 @@ class GreedyQuestions
 		}
 		cout<<"Amount stored = "<<res;
 	}
+	static bool my_sort_JSP(jobs a,jobs b)
+	{
+		return a.profit>b.profit;
+	}
+	void job_sequencing_problem(vector<jobs> &jb)
+	{
+		sort(jb.begin(),jb.end(),my_sort_JSP);
+		int max_deadline=-1,prof=0;
+		for(auto j:jb)
+		{
+			max_deadline=max(max_deadline,j.deadline);
+		}
+		vector<jobs>ans(max_deadline);
+		ans[jb[0].deadline-1]=jb[0];
+		for(int i=1;i<jb.size();i++)
+		{
+			int x=1;
+			while(jb[i].deadline>=x && ans[jb[i].deadline-x].job_number!=-1)
+			{
+				x++;
+			}
+			if((jb[i].deadline-x)>=0)
+			{
+				ans[jb[i].deadline-x]=jb[i];
+				// prof+=jb[i].profit;
+			}
+		}
+		int tm=0;
+		cout<<"Time\tJNo\tDdln\tProf\n";
+		for(auto j:ans)
+		{
+			cout<<tm<<" - "<<(tm+1)<<"\t";
+			tm++;
+			if(j.profit!=-1)
+			{
+				prof+=j.profit;
+				cout<<j.job_number<<"\t"<<j.deadline<<"\t"<<j.profit<<"\n";
+			}
+			else
+			{
+				cout<<"Nothing Scheduled in this time\n";
+			}
+		}
+		cout<<"Profit made : "<<prof;
+	}
 };
 int main()
 {
@@ -76,6 +137,13 @@ int main()
 	GreedyQuestions obj;
 	// vector<vector<int>> act={{10,20},{20,30},{12,25},{11,20},{20,21},{25,27},{1,27}};
 	// obj.activity_selection_problem(act);
-	vector<vector<int>> wei={{50,600},{20,500},{30,400}};
-	obj.fractional_knapsack_problem(wei,70);
+	// vector<vector<int>> wei={{50,600},{20,500},{30,400}};
+	// obj.fractional_knapsack_problem(wei,70);
+	// vector<jobs> job_seq={jobs(0,2,100), jobs(1,1,50), jobs(2,2,10),jobs(3,1,20),jobs(4,3,30)};
+	vector<jobs> job_seq={jobs(0,4,50),jobs(1,1,5),jobs(2,1,20),jobs(3,5,10),jobs(4,5,80)};
+	// for(auto jb:job_seq)
+	// {
+	// 	cout<<jb.job_number<<"\t"<<jb.deadline<<"\t"<<jb.profit<<"\n";
+	// }
+	obj.job_sequencing_problem(job_seq);
 }
